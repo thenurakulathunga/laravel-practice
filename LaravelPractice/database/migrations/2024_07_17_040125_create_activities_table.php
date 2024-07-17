d25d7294599b905175903c74ba60974b8c9e1caf
Thenura Kulathunga <thenura@thesanmark.com>
2024-07-17 10:04:54 +0530

    Create activities table migration and modal

---
+++ LaravelPractice/database/migrations/2024_07_17_040125_create_activities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('activity', length: 225);
+            $table->text('content')->nullable();
+            $table->integer('user_id');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
