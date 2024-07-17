5b3095d4d9608df21e51dfd3c58a58b473e38664
Thenura Kulathunga <thenura@thesanmark.com>
2024-07-17 09:30:17 +0530

    Create error-logs table migration and modal

---
+++ LaravelPractice/database/migrations/2024_07_17_035903_create_error_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('error_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('caption',length: 225);
+            $table->text('content');
+            $table->integer('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('error_logs');
+    }
+};
