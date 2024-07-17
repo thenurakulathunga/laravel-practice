0477e4aa4881206f2ab512fb2f7a178044387241
Thenura Kulathunga <thenura@thesanmark.com>
2024-07-17 09:01:48 +0530

    Create task table migration and modal

---
+++ LaravelPractice/database/migrations/2024_07_17_032157_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('task_title', length: 225)->default('Untitled task');
+            $table->date('due_date')->nullable();
+            $table->integer('user_id');
+            $table->integer('duration')->nullable();
+            $table->boolean('display_others')->default(false);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
